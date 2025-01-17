a3ded9094cb35a08cdfbc5270b7554c4f02a716f
Yasir Ali <yaseenyasir786110@gmail.com>
2025-01-17 11:31:02 -0800

    make the controller of the user and models and routes

---
+++ Server/Controllers/user.controller.js
@@ -0,0 +1,10 @@
+import  bcrypt from 'bcrypt';
+import  jwt from 'jsonwebtoken';
+import User from '../Models/user.model.js';
+
+const signIn =async (req, res) => {
+    
+
+
+}
+export { signIn };
--- Server/index.js
+++ Server/index.js
@@ -1,22 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import cors from 'cors';
 
 
 const app = express();
+dotenv.config();
 
 // Middleware
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({  extended: true }));
+app.use(bodyParser.urlencoded({  extended: true }));
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello to Memories API');
-}
-);
+// Routes
+import userRoutes from './Routes/user.route.js';
+app.use('/api', userRoutes);
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 }
 );
